a44118f3b70ae5f67a63063220e6c6bc723eeb12
Felipe Cechin <ficechin@hotmail.com>
2022-08-05 15:16:13 -0300

    projeto concluded

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,42 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+    });
+
+    console.log(user.id);
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const userFounded = this.users.find((user) => user.id === id);
+    return userFounded;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const userFounded = this.users.find((user) => user.email === email);
+    return userFounded;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = this.findById(receivedUser.id);
+
+    user.admin = true;
+    user.updated_at = new Date();
+
+    return user;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,32 @@
 import { Request, Response } from "express";
+import { IncomingHttpHeaders } from "http";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+interface ICustomHeaders {
+  user_id?: string;
+}
+
+interface IReqCustom<THeader> extends Request {
+  headers: IncomingHttpHeaders & THeader;
+}
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
+  handle(request: IReqCustom<ICustomHeaders>, response: Response): Response {
+    const { user_id } = request.headers;
+
+    if (!user_id) {
+      throw new Error("Must send a user ID");
+    }
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).send({ error: "Error" });
+    }
   }
 }
 
